c3bd4750c2d45e7fe8611938496f6126cb085701
Sean Davis <sean.dev.davis@gmail.com>
2024-10-22 19:46:09 -0500

    added case reference to list

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# prompt for user
-user_prompt = 'Enter a task: '
-
 todos = []
 
-while 2 > 1:
-    todo = input(user_prompt)
-    todos.append(todo)
-    print(todos)
+while True:
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a task: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+
+print("Bye!")
 
 
