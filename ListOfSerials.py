75af9a118da335029f2c27145464669896362c6e
Sean Davis <sean.dev.davis@gmail.com>
2024-10-25 08:58:50 -0500

    tuple

--- ListOfSerials.py
+++ ListOfSerials.py
@@ -3,10 +3,17 @@
 # serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
 # print(serials[2])
 
-# for loop
+# # for loop
 filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
+print(filenames)
+print(type(filenames))
+#
+# for filename in filenames:
+#     newName = filename.replace(".", "-", 1)
+#
+#     print(newName)
 
-for filename in filenames:
-    newName = filename.replace(".", "-", 1)
-
-    print(newName)
+# tuple
+filenames2 = ("1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt")
+print(filenames2)
+print(type(filenames2))
