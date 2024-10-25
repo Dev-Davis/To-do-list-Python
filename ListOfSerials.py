2899672c9d1ff11d0f1f9f1fb1f5ff0f1de9ab2b
Sean Davis <sean.dev.davis@gmail.com>
2024-10-25 10:18:17 -0500

    challenge practice

--- ListOfSerials.py
+++ ListOfSerials.py
@@ -1,4 +1,12 @@
 # # print the 3rd serial number from the list
+
+# mood = "Happy"
+# strength = 99.9
+# rank = 1
+#
+# print(type(mood))
+# print(type(strength))
+# print(type(rank))
 #
 # serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
 # print(serials[2])
@@ -17,10 +25,14 @@
 # filenames2 = ("1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt")
 # print(filenames2)
 # print(type(filenames2))
+#
+# multiple tuples
+# color_codes = (("blue", "7", "third_index"),("red", 4, False),("green", 9, "orange"))
+# print(color_codes)
 
 # append
-seconds = [1.23, 1.45, 1.02]
-current = 1.11
-
-seconds.append(current)
-print(seconds)
+# seconds = [1.23, 1.45, 1.02]
+# current = 1.11
+#
+# seconds.append(current)
+# print(seconds)
