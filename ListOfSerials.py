e2dba51e8e3851f739abc1a62d95f376ee7f5ce2
Sean Davis <sean.dev.davis@gmail.com>
2024-10-25 08:32:04 -0500

    string replace in for loop

--- ListOfSerials.py
+++ ListOfSerials.py
@@ -1,4 +1,12 @@
-# print the 3rd serial number from the list
+# # print the 3rd serial number from the list
+#
+# serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+# print(serials[2])
 
-serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
-print(serials[2])
+# for loop
+filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
+
+for filename in filenames:
+    newName = filename.replace(".", "-", 1)
+
+    print(newName)
